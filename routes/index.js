e27e603a5286ed8ec604ae893b7fc25ca26b8289
Dalian Grullon <dalian.business@aol.com>
2022-07-24 22:11:50 -0400

    Added useful commenting to index.js to enhance reusability

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const notesRouter = require('./notes');
 
 const app = express();
 
+// makes /notes default initial location to all routes implemented in notesRouter module
 app.use('/notes', notesRouter);
 
 module.exports = app;
