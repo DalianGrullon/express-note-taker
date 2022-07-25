c2157a62a23821e2003426a9ae9942886f76d1b4
Dalian Grullon <dalian.business@aol.com>
2022-07-24 22:10:18 -0400

    Added useful commenting to server.js file to enhance reusability

--- server.js
+++ server.js
@@ -11,15 +11,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
+// sends users the notes.html file when navigating to /notes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+// sends users the index.html file when navigating to all other unspecified pages
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// remove link from consolelog once deployed to heroku
+// TODO: remove link from consolelog once deployed to heroku
 app.listen(PORT, () => {
     console.log(`Server is live and listening on http://localhost:${PORT}`);
 });
